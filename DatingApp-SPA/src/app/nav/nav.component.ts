68583054001601b43938139c6632179100373819
Khurram.Enam <Khurram.Enam@DESKTOP-5ABGGOH>
2020-03-31 11:49:48 +1100

    End Of Section 6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,35 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { NotExpr } from '@angular/compiler';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+export class NavComponent  {
 model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
-  ngOnInit() {}
   login() {
   this.authService.login(this.model).subscribe( next => {
-      console.log('Logged in successfully....');
+      this.alertify.success('Logged in successfully....');
   }, error => {
-      console.log('failed to login......');
+      this.alertify.error(error);
   });
 
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !! token;
+    return this.authService.loggedIn();
   }
 
   logout() {
 
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 
 
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,6 +17,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Http;
+using DatingApp.API.Helpers;
+
 
 namespace DatingApp.API
 {
@@ -57,8 +62,20 @@ namespace DatingApp.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler( builder => {
+                builder.Run( async context => {
 
-           // app.UseHttpsRedirection();
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    var errorFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    context.Response.AddApplicationError(errorFeature.Error.Message);
+                    await context.Response.WriteAsync(errorFeature.Error.Message);
+                 });
+               });
+            }
+
+           // app.UseHttp`sRedirection();
 
             app.UseRouting();
 
