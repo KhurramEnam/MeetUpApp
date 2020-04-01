71b57e36ace9c8f8109241673149c5551ff1ce70
Khurram.Enam <Khurram.Enam@DESKTOP-5ABGGOH>
2020-04-02 00:26:06 +1100

    End Of Section 7

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { NotExpr } from '@angular/compiler';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,25 +11,29 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class NavComponent  {
 model: any = {};
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   login() {
   this.authService.login(this.model).subscribe( next => {
       this.alertify.success('Logged in successfully....');
   }, error => {
       this.alertify.error(error);
+  }, () => {
+    this.router.navigate(['/members']);
   });
 
   }
 
   loggedIn() {
     return this.authService.loggedIn();
+    this.alertify.message('logged out');
   }
 
   logout() {
 
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
 
